0326cc772ae4a84cb6d7bd0c53a2dd0737c9ba86
muhammadsufiyanbaig <BDA24S001@stu.smiu.edu.pk>
2024-12-23 16:28:50 +0500

    fix gender issue

--- controllers/authController.js
+++ controllers/authController.js
@@ -48,7 +48,7 @@ const SignUp = async (req, res) => {
         pattern: "dd-mm-yyyy",
       });
     }
-    if (gender !== "male" && Gender !== "female") {
+    if (gender !== "male" && gender !== "female") {
       return res
         .status(400)
         .json({ message: "Gender must be either 'male' or 'female'" });
