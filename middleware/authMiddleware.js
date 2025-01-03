7f878e1d5dceaac2fc43bb48d275054cb7e22a0e
muhammadsufiyanbaig <BDA24S001@stu.smiu.edu.pk>
2025-01-03 19:37:51 +0500

    middleware logs

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -2,10 +2,12 @@ const jwt = require('jsonwebtoken');
 
 function authenticateToken(req, res, next) {
   const token = req.cookies.token || req.body.token || req.query.token;
-
+  console.log('token', token);
+  
   if (!token) return res.status(401).json({ error: 'Unauthorized access' });
 
   jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
+
     if (err) return res.status(403).json({ error: 'Token is invalid or expired' });
     req.user = user;
     next();
